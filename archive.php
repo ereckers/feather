36b4bc970fd95ee19b59fb8731aefd23fd5ff4c1
Ed Reckers <ed@redbridgenet.com>
2014-06-03 11:25:09 -0700

    tab formatting experiment for github ts=4

--- archive.php
+++ archive.php
@@ -10,8 +10,8 @@
 get_header(); ?>
 
 <div class="wrap container" role="document">
-    <div class="content row">
-        <main class="main col-sm-8" role="main">
+	<div class="content row">
+		<main class="main col-sm-8" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
@@ -21,11 +21,11 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'templates/content', get_post_format() );
+				/* Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'templates/content', get_post_format() );
 				?>
 
 			<?php endwhile; ?>
@@ -36,13 +36,13 @@ get_header(); ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 		<?php endif; ?>
 
-        </main><!--/ .main -->
+		</main><!--/ .main -->
 
-        <aside class="sidebar col-sm-4" role="complementary">
-            <?php get_sidebar(); ?>
-        </aside><!--/ .sidebar -->
+		<aside class="sidebar col-sm-4" role="complementary">
+			<?php get_sidebar(); ?>
+		</aside><!--/ .sidebar -->
 
-    </div><!--/ .content .row -->
+	</div><!--/ .content .row -->
 </div><!--/ .wrap .container -->
 
 <?php get_footer(); ?>
--- author.php
+++ author.php
@@ -10,47 +10,47 @@
 get_header(); ?>
 
 <div class="wrap container" role="document">
-    <div class="content row">
-        <main class="main col-sm-8" role="main">
+	<div class="content row">
+		<main class="main col-sm-8" role="main">
 
 		<?php if ( have_posts() ) : ?>
 
 			<header class="archive-header">
 				<h1 class="archive-title">
-                    <?php
-                        /*
-                         * Queue the first post, that way we know what author
-                         * we're dealing with (if that is the case).
-                         *
-                         * We reset this later so we can run the loop properly
-                         * with a call to rewind_posts().
-                         */
-                        the_post();
+				<?php
+				/*
+				 * Queue the first post, that way we know what author
+				 * we're dealing with (if that is the case).
+				 *
+				 * We reset this later so we can run the loop properly
+				 * with a call to rewind_posts().
+				 */
+				the_post();
 
-                        printf( __( 'All posts by %s', THEME_TEXTDOMAIN ), get_the_author() );
-                    ?>
-                </h1>
+				printf( __( 'All posts by %s', THEME_TEXTDOMAIN ), get_the_author() );
+				?>
+				</h1>
 				<?php //get_template_part( 'templates/author-meta' ); ?>
 			</header><!-- .archive-header -->
 
-            <?php
-				/*
-				 * Since we called the_post() above, we need to rewind
-				 * the loop back to the beginning that way we can run
-				 * the loop properly, in full.
-				 */
-				rewind_posts();
+			<?php
+			/*
+			 * Since we called the_post() above, we need to rewind
+			 * the loop back to the beginning that way we can run
+			 * the loop properly, in full.
+			 */
+			rewind_posts();
 
-				// Start the Loop
-				while ( have_posts() ) : the_post(); ?>
+			// Start the Loop
+			while ( have_posts() ) : the_post(); ?>
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'templates/content', get_post_format() );
-				?>
+			<?php
+			/* Include the Post-Format-specific template for the content.
+			 * If you want to override this in a child theme, then include a file
+			 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+			 */
+			get_template_part( 'templates/content', get_post_format() );
+			?>
 
 			<?php endwhile; ?>
 
@@ -60,13 +60,13 @@ get_header(); ?>
 			<?php get_template_part( 'templates/content', 'none' ); ?>
 		<?php endif; ?>
 
-        </main><!--/ .main -->
+		</main><!--/ .main -->
 
-        <aside class="sidebar col-sm-4" role="complementary">
-            <?php get_sidebar(); ?>
-        </aside><!--/ .sidebar -->
+		<aside class="sidebar col-sm-4" role="complementary">
+			<?php get_sidebar(); ?>
+		</aside><!--/ .sidebar -->
 
-    </div><!--/ .content .row -->
+	</div><!--/ .content .row -->
 </div><!--/ .wrap .container -->
 
 <?php get_footer(); ?>
