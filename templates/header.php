fc2bc5a40aaeb761e2cd6592339b7bdbf50359a8
Ed Reckers <ed@redbridgenet.com>
2014-06-15 15:08:01 -0700

    markup formatting in header template file

--- templates/header.php
+++ templates/header.php
@@ -10,11 +10,9 @@
 			<a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
 		</div>
 		<nav class="collapse navbar-collapse" role="navigation">
-		<?php
-		if ( has_nav_menu('primary') ) :
-			wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'nav navbar-nav'));
-		endif;
-		?>
+		<?php if ( has_nav_menu('primary') ) : ?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav navbar-nav' ) ); ?>
+		<?php endif; ?>
 		</nav>
 	</div>
 </header>
