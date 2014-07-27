84058ca8055e300def8359bc7f952323fa87514a
Ed Reckers <ed@redbridgenet.com>
2014-07-27 12:45:41 -0700

    allow for wp-paginate plugin for pagination

--- templates/pagination.php
+++ templates/pagination.php
@@ -1,8 +1,13 @@
-		<?php if ( $wp_query->max_num_pages > 1 ) : ?>
-			<nav class="post-nav">
-				<ul class="pager">
-					<li class="previous"><?php next_posts_link( __( '&larr; Older posts', THEME_TEXTDOMAIN ) ); ?></li>
-					<li class="next"><?php previous_posts_link( __( 'Newer posts &rarr;', THEME_TEXTDOMAIN ) ); ?></li>
-				</ul>
-			</nav>
-		<?php endif; ?>
+<?php if ( function_exists('wp_paginate') ) : ?>
+	<?php wp_paginate(); ?>
+<?php else: ?>
+	<?php if ( $wp_query->max_num_pages > 1 ) : ?>
+		<nav class="post-nav">
+			<ul class="pager">
+				<li class="previous"><?php next_posts_link( __( '&larr; Older posts', THEME_TEXTDOMAIN ) ); ?></li>
+				<li class="next"><?php previous_posts_link( __( 'Newer posts &rarr;', THEME_TEXTDOMAIN ) ); ?></li>
+			</ul>
+		</nav>
+	<?php endif; ?>
+<?php endif; ?>
+
