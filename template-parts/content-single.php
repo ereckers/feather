0125589fef8a8e42870fb15a2e6447da0d170059
Ed Reckers <ed@redbridgenet.com>
2014-11-08 11:37:08 -0800

    fix filepath templates to template-parts

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -2,16 +2,16 @@
   <article <?php post_class(); ?>>
     <header>
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+      <?php get_template_part('template-parts/entry-meta'); ?>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
     </div><!--/ .entry-content -->
 	<?php the_tags('<ul class="nav nav-pills"><li>','</li><li>','</li></ul>'); ?>
-	<?php get_template_part( 'templates/author-meta' ); ?>
+	<?php get_template_part( 'template-parts/author-meta' ); ?>
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', THEME_TEXTDOMAIN ), 'after' => '</p></nav>')); ?>
     </footer>
-    <?php comments_template('/templates/comments.php'); ?>
+    <?php comments_template('/template-parts/comments.php'); ?>
   </article>
 <?php endwhile; ?>
