2c15383f9dafc1a5f91d278e2d051aaa976e729f
Ed Reckers <ed@redbridgenet.com>
2014-11-08 11:56:18 -0800

    fix templates => template-parts filepath in roots cleanup.php

--- lib/roots/cleanup.php
+++ lib/roots/cleanup.php
@@ -252,11 +252,11 @@ function roots_request_filter($query_vars) {
 add_filter('request', 'roots_request_filter');
 
 /**
- * Tell WordPress to use searchform.php from the templates/ directory
+ * Tell WordPress to use searchform.php from the template-parts/ directory
  */
 function roots_get_search_form($form) {
   $form = '';
-  locate_template('/templates/searchform.php', true, false);
+  locate_template('/template-parts/searchform.php', true, false);
   return $form;
 }
 add_filter('get_search_form', 'roots_get_search_form');
